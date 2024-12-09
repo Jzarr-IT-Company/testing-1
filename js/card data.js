5e719ccc449284aebe0593b840376ee994820b18
Jzarr-software-house <davidwandavid18@gmail.com>
2024-12-09 11:35:39 +0500

    working

---
+++ js/card data.js
@@ -0,0 +1,13 @@
+var productsArray = [
+
+    { product: "thunderbolt roller ride ", view:"Thunderbolt Twister: An electrifying roller coaster ride filled with thrilling twists and turns" , image: "Saved Pictures/img (1).jpg", price: "15$" },
+    { product: "sty thunderbolt roller ride", view:"Sty Thunderbolt Roller Ride: A pulse-pounding, stylish roller coaster adventurepromising adrenaline-pumping ", image: "Saved Pictures/img (1).webp", price: "20$" },
+    { product: "roller ride", view:"Roller Ride: An exhilarating journey of twists, turns, and adrenaline rushes",image: "Saved Pictures/img (2).jpg", price: "45$" },
+    { product: "Dizzy Heights Drop", view:"Roller Ride: A thrilling journey of twists and Roller Ride: Brace yourself for an exhilarating adventure as you soar ", image: "Saved Pictures/img (2).webp", price: "20$" },
+    { product: "skye ride", view:"Roller Ride: Brace yourself for an exhilarating adventure as you soar through loops and drops, experiencing heart-pounding excitement ", image: "Saved Pictures/img (3).jpg", price: "90$" },
+    { product: "Jungle Safari Soaker",  view:"Jungle Safari Soaker: Dive into a wild adventure where you'll navigate through lush ",image: "Saved Pictures/img12.jpg", price: "10$" },
+    { product: "Dragon's Den Dive",  view:": Embark on a daring plunge into the depths of adventure amidst the fiery realm of dragons.", image: "Saved Pictures/img (5).jpg", price: "29$" },
+    { product: "Supersonic Odyssey ", view:"Supersonic Odyssey: Embark  through twists and loops, experiencing the thrill of velocity at its peak", image: "Saved Pictures/img13.jpg", price: "80$" },  
+    { product: "Enchanted Castle",  view:"Enchanted Castle: Enter a realm of magic and wonder, where every corner holds mysteries and adventures waiting ",image: "Saved Pictures/img (7).jpg", price: "100$" },
+   
+  ];
